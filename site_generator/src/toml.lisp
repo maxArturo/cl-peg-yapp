ebd999301da8b15608299771e403ce4902850774
Max Alcala <maxarturo@gmail.com>
2023-08-19 12:05:21 -0300

    feat: refactor & add tests for toml

--- site_generator/src/toml.lisp
+++ site_generator/src/toml.lisp
@@ -11,19 +11,38 @@
   (declare (string str))
   (equal "" str))
 
+#+5am
+(5am:test is-empty-test
+    (5am:is (eq NIL (is-empty "funky")))
+    (5am:is (eq T (is-empty ""))))
+
 (defun is-comment (str)
   "returns T if str is considered a comment, NIL otherwise"
   (equal #\# (uiop:first-char (string-left-trim " " str))))
 
 #+5am
 (5am:test is-comment-test
-    (5am:is (eq NIL (is-comment "funky")))
-    (5am:is (eq NIL (is-comment " # now this is funky"))))
+  (for:for 
+    (((&key expected input) in 
+        '((:expected T :input "# ")
+          (:expected NIL :input " hello ")
+          (:expected T :input " # hell "))))
+    (5am:is (equal expected (is-comment input)))))
 
-(defun trim (str)
+(defun trim (str &key (extra-chars ""))
   "simple empty space trim"
-  (declare (string str))
-  (string-trim " " str))
+  (declare (string str extra-chars))
+  (string-trim (concatenate 'string " " extra-chars) str))
+
+#+5am
+(5am:test trim-test
+  (for:for 
+    (((&key expected input extra-chars) in 
+        '((:expected "" :input " ")
+          (:expected "hello" :input " hello         ")
+          (:expected "chancho.md" :input "\"chancho.md\"" :extra-chars "\"")
+          (:expected "hello         ." :input " hello         . " :extra-chars "\""))))
+    (5am:is (equal expected (trim input :extra-chars (or extra-chars ""))))))
 
 (defun get-valid-keyval (str)
   "returns a plist for a valid simple TOML key-value string of the form
@@ -33,15 +52,28 @@
        (cond ((and 
          (not (is-empty key))
          (not (is-empty val))) 
-              (list (read-from-string (trim key))  (trim val)))
-             ('t nil)))))
+              (list (read-from-string (concatenate 'string ":" (trim key)))  
+                    (trim val :extra-chars "\"")))
+             ('t NIL)))))
 
+#+5am
+(5am:test get-valid-keyval-test
+  (for:for 
+    (((&key expected input) in 
+        '((:expected NIL :input " ")
+          (:expected (:hello "chancho.md") :input " hello = \"chancho.md\"")
+          (:expected NIL :input " hello  \"chancho.md\"")
+          (:expected (:location "/usr/share/bin.bin") :input "location = \"/usr/share/bin.bin\"")
+          (:expected (:bar "foo.baz") :input " bar = \" foo.baz\""))))
+    (cond ((eq NIL expected) (5am:is (eq NIL (get-valid-keyval input))))
+          (T (5am:is (equal (get-valid-keyval input) expected))))))
 
 (defun toml-to-plist (file-list-input)
     (for:for ((raw-lines in file-list-input)
-          (lines unless (equal "" raw-lines) = raw-lines)
-          (splits = (uiop:split-string lines :separator "="))
+          (lines unless (is-empty raw-lines) = raw-lines)
+          (splits unless (is-comment lines) = (get-valid-keyval lines))
           (key-values 
+            unless (eq NIL splits)
             = (destructuring-bind (key-str val-str) splits 
                 (list (read-from-string (string-trim " " key-str)) (string-trim "\" " val-str))))
           (ans reducing key-values :by (lambda (curr-val acc) (append curr-val acc))))))
--- site_generator/src/useful_packages.lisp
+++ site_generator/src/useful_packages.lisp
@@ -1,4 +1,6 @@
-;; (load "~/quicklisp/setup.lisp")
-;; (ql:quickload "trivia") for pattern matching
-;; (ql:quickload "for") structured looping
+(load "~/quicklisp/setup.lisp")
+(ql:quickload "trivia") for pattern matching
+(ql:quickload "for") structured looping
+(load "~/Developer/hard_lisp/site_generator/sitegen.asd")
+(5am:run! 'sitegen:sitegen-tests)
 
